35261c0a3a27861417e88c690163236f8b213bc7
ram4ik <ram4ik@hotmail.com>
2023-10-30 19:47:43 +0200

    sharing an observed object with a new view

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -21,6 +21,7 @@ class Expenses {
 
 struct ContentView: View {
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
    
     var body: some View {
         NavigationStack {
@@ -33,9 +34,11 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
