dab2723fe4d0e6df04bdea1a2a7af3b56a63775e
ram4ik <ram4ik@hotmail.com>
2023-10-31 07:32:54 +0200

    move to separate classes

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,35 +7,6 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable, Codable {
-    var id: UUID = UUID()
-    let name: String
-    let type: String
-    let amount: Double
-}
-
-@Observable
-class Expenses {
-    var items = [ExpenseItem]() {
-        didSet {
-            if let encoded = try? JSONEncoder().encode(items) {
-                UserDefaults.standard.setValue(encoded, forKey: "Items")
-            }
-        }
-    }
-    
-    init() {
-        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
-            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
-                items = decodedItems
-                return
-            }
-        }
-        
-        items = []
-    }
-}
-
 struct ContentView: View {
     @State private var expenses = Expenses()
     @State private var showingAddExpense = false
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,30 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by admin on 10/31/23.
+//
+
+import Foundation
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.setValue(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+}
